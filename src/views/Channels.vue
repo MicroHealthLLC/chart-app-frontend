077dbf7f810958ac7caa77c670fbc77230bae5e2
Christopher Calderon <christopher.calderon@microhealthllc.com>
2021-08-04 23:22:19 -0700

    Add data and tables for Data Sets and Channels pages

--- src/views/Channels.vue
+++ src/views/Channels.vue
@@ -1,13 +1,86 @@
 <template>
-  <h1>Channels</h1>
+  <v-row>
+    <v-col>
+      <div class="d-flex justify-space-between">
+        <h3>Channels</h3>
+        <v-btn class="mb-2" color="primary" small to="/add-data-set"
+          >Add Channel <v-icon small>mdi-plus</v-icon></v-btn
+        >
+      </div>
+
+      <v-divider class="mb-4"></v-divider>
+      <v-card>
+        <v-data-table :headers="headers" :items="channels">
+          <template v-slot:item.actions="{ item }">
+            <v-icon color="primary" small class="mr-2" @click="editItem(item)">
+              mdi-pencil
+            </v-icon>
+            <v-icon color="primary" small @click="deleteItem(item)">
+              mdi-delete
+            </v-icon>
+          </template>
+        </v-data-table>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
-export default {
+import { mapActions, mapGetters } from "vuex";
 
-}
+export default {
+  name: "Channels",
+  data() {
+    return {
+      headers: [
+        {
+          text: "Title",
+          sortable: true,
+          value: "title",
+        },
+        {
+          text: "Date Added",
+          sortable: true,
+          value: "created_at",
+        },
+        {
+          text: "Category",
+          sortable: true,
+          value: "category",
+        },
+        {
+          text: "Description",
+          sortable: false,
+          value: "description",
+        },
+        {
+          text: "Actions",
+          sortable: false,
+          value: "actions",
+        },
+      ],
+    };
+  },
+  computed: {
+    ...mapGetters(["channels"]),
+  },
+  methods: {
+    ...mapActions(["fetchChannels"]),
+    editItem(item) {
+      console.log(`EDIT ${item.title}`);
+    },
+    deleteItem(item) {
+      console.log(`DELETE ${item.title}`);
+    },
+  },
+  beforeMount() {
+    this.fetchChannels();
+  },
+};
 </script>
 
 <style>
-
+td.text-start:first-child {
+  font-weight: 600;
+}
 </style>
--- src/views/DataSets.vue
+++ src/views/DataSets.vue
@@ -1,13 +1,77 @@
 <template>
-  <h1>DATA SETS</h1>
+  <v-row>
+    <v-col>
+      <div class="d-flex justify-space-between">
+        <h3>Data Sets</h3>
+        <v-btn class="mb-2" color="primary" small to="/add-data-set"
+          >Add Data Set <v-icon small>mdi-plus</v-icon></v-btn
+        >
+      </div>
+
+      <v-divider class="mb-4"></v-divider>
+      <v-card>
+        <v-data-table :headers="headers" :items="dataSets">
+          <template v-slot:item.actions="{ item }">
+            <v-icon color="primary" small class="mr-2" @click="editItem(item)">
+              mdi-pencil
+            </v-icon>
+            <v-icon color="primary" small @click="deleteItem(item)">
+              mdi-delete
+            </v-icon>
+          </template>
+        </v-data-table>
+      </v-card>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
-export default {
+import { mapActions, mapGetters } from "vuex";
 
-}
+export default {
+  name: "DataSets",
+  data() {
+    return {
+      headers: [
+        {
+          text: "Title",
+          sortable: true,
+          value: "title",
+        },
+        {
+          text: "Date Added",
+          sortable: true,
+          value: "created_at",
+        },
+        {
+          text: "Description",
+          sortable: false,
+          value: "description",
+        },
+        {
+          text: "Actions",
+          sortable: false,
+          value: "actions",
+        },
+      ],
+    };
+  },
+  computed: {
+    ...mapGetters(["dataSets"]),
+  },
+  methods: {
+    ...mapActions(["fetchDataSets"]),
+    editItem(item) {
+      console.log(`EDIT ${item.title}`);
+    },
+    deleteItem(item) {
+      console.log(`DELETE ${item.title}`);
+    },
+  },
+  beforeMount() {
+    this.fetchDataSets();
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
