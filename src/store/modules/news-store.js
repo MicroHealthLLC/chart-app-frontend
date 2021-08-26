f3a3a9228e56b21bf61e21c151ecd9fbedda816c
Christopher Calderon <christopher.calderon@microhealthllc.com>
2021-08-26 09:14:33 -0700

    Add News view and store

--- src/store/modules/news-store.js
+++ src/store/modules/news-store.js
@@ -3,6 +3,10 @@ import axios from "axios";
 export default {
   state: {
     news: [],
+    activeNews: {
+      title: "",
+      body: "",
+    },
   },
   actions: {
     fetchNews({ commit }) {
@@ -13,9 +17,46 @@ export default {
         commit("SET_NEWS", res.data);
       });
     },
+    addNews({ commit }, news) {
+      axios({
+        method: "POST",
+        url: "http://localhost:3000/v1/news",
+        data: news,
+      }).then((res) => {
+        commit("ADD_NEWS", res.data);
+        commit("SET_SNACKBAR", {
+          show: true,
+          message: "News added successfully!",
+        });
+      });
+    },
+    updateNews({ commit }, news) {
+      axios({
+        method: "PATCH",
+        url: `http://localhost:3000/v1/news/${news.id}`,
+        data: news,
+      }).then((res) => {
+        commit("UPDATE_NEWS", res.data);
+        commit("SET_SNACKBAR", {
+          show: true,
+          message: "News updated successfully!",
+        });
+      });
+    },
   },
   mutations: {
     SET_NEWS: (state, news) => (state.news = news),
+    SET_ACTIVE_NEWS: (state, news) => (state.activeNews = news),
+    ADD_NEWS: (state, news) => state.news.unshift(news),
+    UPDATE_NEWS: (state, news) => {
+      let index = state.news.findIndex(
+        (newsReport) => newsReport.id == news.id
+      );
+      state.news[index] = news;
+    },
   },
-  getters: { news: (state) => state.news },
+  getters: {
+    news: (state) => state.news,
+    activeNews: (state) => state.activeNews,
+  },
 };
--- src/views/News.vue
+++ src/views/News.vue
@@ -1,13 +1,123 @@
 <template>
-  <h1>News</h1>
+  <v-row>
+    <v-col>
+      <div class="d-flex justify-space-between">
+        <h3>News</h3>
+        <v-btn class="mb-2" @click="openDialog" color="primary" small
+          >Add News <v-icon small>mdi-plus</v-icon></v-btn
+        >
+      </div>
+
+      <v-divider class="mb-4"></v-divider>
+
+      <v-card>
+        <v-data-table :headers="headers" :items="news">
+          <template v-slot:item.actions="{ item }">
+            <v-icon color="primary" small class="mr-2" @click="editItem(item)">
+              mdi-pencil
+            </v-icon>
+            <v-icon color="primary" small @click="deleteItem(item)">
+              mdi-delete
+            </v-icon>
+          </template>
+        </v-data-table>
+      </v-card>
+      <!-- Modal form for news -->
+      <v-dialog v-model="dialog" width="50%">
+        <v-card>
+          <v-toolbar color="info" dark>
+            <h3 v-if="activeNews.id">Update News</h3>
+            <h3 v-else>Add News</h3>
+          </v-toolbar>
+          <v-card-text>
+            <v-text-field
+              v-model="activeNews.title"
+              label="Title"
+            ></v-text-field>
+            <v-textarea v-model="activeNews.body" label="Body"></v-textarea>
+          </v-card-text>
+          <v-card-actions class="justify-end">
+            <v-btn @click="closeDialog" outlined small>Close</v-btn>
+            <v-btn @click="saveNews" color="primary" depressed small
+              >Save</v-btn
+            >
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </v-col>
+  </v-row>
 </template>
 
 <script>
-export default {
+import { mapActions, mapGetters, mapMutations } from "vuex";
 
-}
+export default {
+  name: "News",
+  data() {
+    return {
+      dialog: false,
+      headers: [
+        {
+          text: "Title",
+          sortable: true,
+          value: "title",
+          width: "25%",
+        },
+        {
+          text: "Date Added",
+          sortable: true,
+          value: "updated_at",
+        },
+        {
+          text: "Body",
+          sortable: false,
+          value: "body",
+          width: "50%",
+        },
+        {
+          text: "Actions",
+          sortable: false,
+          value: "actions",
+        },
+      ],
+    };
+  },
+  computed: {
+    ...mapGetters(["activeNews", "news"]),
+  },
+  methods: {
+    ...mapActions(["addNews", "fetchNews", "updateNews"]),
+    ...mapMutations(["SET_ACTIVE_NEWS"]),
+    openDialog() {
+      this.dialog = true;
+    },
+    closeDialog() {
+      this.dialog = false;
+      this.SET_ACTIVE_NEWS({
+        title: "",
+        body: "",
+      });
+    },
+    editItem(item) {
+      console.log(`EDIT ${item.title}`);
+      this.SET_ACTIVE_NEWS(item);
+      this.dialog = true;
+    },
+    deleteItem(item) {
+      console.log(`DELETE ${item.title}`);
+    },
+    saveNews() {
+      if (this.activeNews.id) {
+        this.updateNews(this.activeNews);
+      } else {
+        this.addNews(this.activeNews);
+      }
+    },
+  },
+  beforeMount() {
+    this.fetchNews();
+  },
+};
 </script>
 
-<style>
-
-</style>
+<style></style>
