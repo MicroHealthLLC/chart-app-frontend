a9f41b4338d0c14db7efaf335a00d898af044414
Zach Egner <zach.egner@microhealthllc.com>
2022-12-06 21:29:09 -0500

    changes icon of view and adds delete permission

--- src/views/DataSets.vue
+++ src/views/DataSets.vue
@@ -3,14 +3,14 @@
     <v-col>
       <div class="d-flex justify-space-between">
         <h3>Data Sets</h3>
-        <v-btn class="mb-2" color="primary" small to="/add-data-set"
-          >Add Data Set <v-icon small>mdi-plus</v-icon></v-btn
-        >
+        <v-btn class="mb-2" color="primary" small to="/add-data-set">Add Data Set <v-icon
+            small>mdi-plus</v-icon></v-btn>
       </div>
 
       <v-divider class="mb-4"></v-divider>
       <v-card>
         <v-data-table :headers="headers" :items="dataSets">
+          
           <!-- Formatted Date -->
           <template v-slot:item.created_at="{ item }">
             <span>{{ new Date(item.createdAt).toLocaleDateString() }}</span>
@@ -18,14 +18,26 @@
           <template v-slot:item.user="{ item }">
             <span>{{ item.user }}</span>
           </template>
+
           <!-- Action Buttons -->
           <template v-slot:item.actions="{ item }">
-            <v-icon color="primary" small class="mr-2" @click="editItem(item)">
-              mdi-pencil
-            </v-icon>
-            <v-icon color="primary" small @click="deleteItem(item)">
-              mdi-delete
-            </v-icon>
+            <v-tooltip top>
+              <template v-slot:activator="{ on, attrs }">
+                <v-icon color="primary" small class="mr-2" @click="editItem(item)" v-bind="attrs" v-on="on">
+                  mdi-table-eye
+                </v-icon>
+              </template>
+              <span>View</span>
+            </v-tooltip>
+            <v-tooltip top>
+              <template v-slot:activator="{ on, attrs }">
+                <v-icon v-if="(item.user == `${user.attributes.given_name} ${user.attributes.family_name}`)"
+                  color="primary" small @click="deleteItem(item)" v-bind="attrs" v-on="on">
+                  mdi-delete
+                </v-icon>
+              </template>
+              <span>Delete</span>
+            </v-tooltip>
           </template>
         </v-data-table>
       </v-card>
@@ -80,21 +92,20 @@ export default {
       console.log(e)
     },
     editItem(item) {
-      
-      // console.log(item)
-     this.fetchDataSet({id: item.id})
-      this.$router.push(`/data-sets/${item.id}`) 
-      
+      console.log(item)
+      this.fetchDataSet(item.id)
+      this.$router.push(`/data-sets/${item.id}`)
     },
     async deleteItem(item) {
       console.log(item)
-      await this.removeDataSet({id: item.id});
+      await this.removeDataSet({ id: item.id });
     },
   },
   beforeMount() {
     this.fetchDataSets();
   },
   mounted() {
+    console.log(this.user)
     console.log(this.dataSets)
   }
 };
