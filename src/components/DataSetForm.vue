e54a26b1bbc2f6d63ad4b1e6ee4fd20c595db544
Zach Egner <zach.egner@microhealthllc.com>
2023-01-09 16:36:49 -0500

    changes addchannel to modal

--- src/components/DataSetForm.vue
+++ src/components/DataSetForm.vue
@@ -345,7 +345,14 @@ export default {
     resetAndGoBack(){
       this.clear()
       this.$refs.form.reset();
-      this.$router.push(`/${this.currentChannels[0].name}/data-sets`)
+      if (this.$route.path === "/it_apps/data-sets"){
+        this.$emit("closeDataSetForm")
+      } else {
+        this.$router.push(`/${this.currentChannels[0].name}/data-sets`)
+      }
+    },
+    cancelForm() {
+      this.$route.path === '/it_apps/data-sets' ? this.resetAndGoBack() : this.isReadOnly = true
     },
     clear() {
       this.$refs.form.reset();
